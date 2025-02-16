ad5b3feb428645179a8fcf4376dfa028b302e2fe
ganesh0706patil <ganeshpatil208507@gmail.com>
2025-02-16 16:55:24 +0530

    Added the code of Angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,11 +3,12 @@ import { RouterOutlet } from '@angular/router';
 import { LoginAdminComponent } from './components/login-admin/login-admin.component';
 import { LoginUserComponent } from './components/login-user/login-user.component';
 import { DatabindingComponent } from "./components/databinding/databinding.component";
+import { DirectiveComponent } from './components/directive/directive.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [LoginAdminComponent, LoginUserComponent, DatabindingComponent],
+  imports: [LoginAdminComponent, LoginUserComponent, DatabindingComponent, DirectiveComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
