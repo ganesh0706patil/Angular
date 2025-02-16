4dda45839236c436f2a2d99a02eebaf296a6b5b5
ganesh0706patil <ganeshpatil208507@gmail.com>
2025-02-16 17:08:11 +0530

    Project started basic layout created

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,21 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { LoginAdminComponent } from './components/login-admin/login-admin.component';
-import { LoginUserComponent } from './components/login-user/login-user.component';
-import { DatabindingComponent } from "./components/databinding/databinding.component";
-import { DirectiveComponent } from './components/directive/directive.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [LoginAdminComponent, LoginUserComponent, DatabindingComponent, DirectiveComponent],
+  imports: [],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'Tutorial';
-  onClick(todo : string) {
-    console.log("Onclick has been triggered!!")
-    alert("Todo is Successfully deleted!!")
-  }
+  
 }
